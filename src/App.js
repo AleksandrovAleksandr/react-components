54fde8ae8786abcdd916a77293ee2760ec9a5b09
Aleksandr Aleksandrov <aleksandrovfrontend@gmail.com>
2021-04-22 17:19:55 +0300

    Подготовка проекта

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import TextField from './components/TextField/TextField'
 
-function App() {
+import './App.scss'
+const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <TextField
+        id={1}
+        withClearButton
+        placeholder="Введите название вопроса..."
+      />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
